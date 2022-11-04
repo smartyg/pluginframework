2d37860f2bcb669ef263735e8d2b497d2a5f7de7
Martijn Goedhart <goedhart.martijn@gmail.com>
2022-11-04 15:46:02 +0100

    change to cpplogger

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -9,7 +9,7 @@
 #include <filesystem>
 #include <utility>
 #include <dlfcn.h>
-#include <gpsdata/utils/Logger.hpp>
+#include <Logger.hpp>
 
 #include "pluginframework/plugins.h"
 
@@ -18,8 +18,7 @@ using pluginframework::Controller;
 bool is_shared_library (const std::filesystem::directory_entry& e);
 
 bool is_shared_library (const std::filesystem::directory_entry& e) {
-	//DEBUG_MSG ("Check if file for extension: %s\n", e.path ().string ().c_str ());
-	//DEBUG_MSG ("extension is : %s\n", e.path ().extension ().string ().c_str ());
+	DEBUG_2_MSG (1, "Check if file for extension: {:s}\n", e.path ().string ());
 	const auto& path = e.path ();
 	if (!path.has_extension ()) return false;
 	return (path.extension ().string ().compare (".so") == 0);
@@ -32,7 +31,7 @@ Controller& Controller::getInstance (void) {
 }
 
 Controller::~Controller (void) {
-	DEBUG_MSG ("Controller::%s ()\n", __func__);
+	DEBUG_MSG ("Controller::{:s} ()\n", __func__);
 
 	this->_destructor_called = true;
 
@@ -50,14 +49,14 @@ Controller::~Controller (void) {
 	int dl_ret;
 	//for (const auto& [file, handle] : this->_loaded_files) {
 	for (auto it = this->_loaded_files.begin (); it != this->_loaded_files.end (); ++it) {
-		DEBUG_MSG ("Try to close module: %s\n", it->first.c_str ());
+		DEBUG_2_MSG (1, "Try to close module: {:s}\n", it->first);
 		if ((dl_ret = dlclose (it->second)) != 0)
-			WARNING_MSG ("Failed to close module %s: %d\n", it->first.c_str (), dl_ret);
+			WARNING_MSG ("Failed to close module {:s}: {:d}\n", it->first, dl_ret);
 	}
 }
 
 void Controller::addManager (const std::string_view tag, std::function<void(const char*, void*)> registar, std::function<void(const char*)> deregistar) {
-	DEBUG_MSG ("Controller::%s (%s, %p, %p)\n", __func__, tag.data (), registar, deregistar);
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:p}, {:p})\n", __func__, tag.data (), fmt::ptr (&registar), fmt::ptr (&deregistar));
 
 	bool already_present = false;
 	try {
@@ -75,7 +74,7 @@ void Controller::addManager (const std::string_view tag, std::function<void(cons
 }
 
 void Controller::registerPluginToManager (const std::string& tag, const PluginDetails& plugin) const {
-	DEBUG_MSG ("Controller::%s (%s, ...)\n", __func__, tag.c_str ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, ...)\n", __func__, tag);
 
 	try {
 		const auto& manager = this->_managers.at (tag);
@@ -89,9 +88,9 @@ void Controller::scanDirectory (std::string_view directory) {
 	if (!std::filesystem::is_directory (directory_path))
 		return;
 
-	DEBUG_MSG ("Checking files in directory: %s\n", directory_path.c_str ());
+	DEBUG_MSG ("Checking files in directory: {:s}\n", directory_path.string ());
 	for (const auto& dir_entry : std::filesystem::directory_iterator{directory_path}) {
-		DEBUG_MSG ("Check file: %s\n", dir_entry.path ().c_str ());
+		DEBUG_2_MSG (1, "Check file: {:s}\n", dir_entry.path ().string ());
 		if (is_regular_file (dir_entry) && is_shared_library (dir_entry)) {
 			const std::filesystem::path entry_abs = std::filesystem::canonical (dir_entry.path ());
 			// Check if the file is already open
@@ -104,9 +103,9 @@ void Controller::scanDirectory (std::string_view directory) {
 				const std::string path_to_load = entry_abs.string ();
 				void *plugin = dlopen (path_to_load.c_str (), RTLD_NOW);
 				if (!plugin) {
-					WARNING_MSG ("Cannot load module %s: %s\n", path_to_load.c_str (), dlerror ());
+					WARNING_MSG ("Cannot load module {:s}: {:s}\n", path_to_load, dlerror ());
 				} else {
-					DEBUG_MSG ("Loaded module: %s\n", path_to_load.c_str ());
+					DEBUG_2_MSG (1, "Loaded module: {:s}\n", path_to_load);
 					this->_loaded_files.insert (std::make_pair (std::move (path_to_load), plugin));
 				}
 			}
@@ -115,7 +114,7 @@ void Controller::scanDirectory (std::string_view directory) {
 }
 
 bool Controller::hasPluginRegistered (const std::string_view tag, const std::string_view id) {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0) return true;
 	}
@@ -123,7 +122,7 @@ bool Controller::hasPluginRegistered (const std::string_view tag, const std::str
 }
 
 const PluginDetails Controller::getPluginDetails (const std::string_view tag, const std::string_view id) {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0) return entry.second;
 	}
@@ -131,7 +130,7 @@ const PluginDetails Controller::getPluginDetails (const std::string_view tag, co
 }
 
 void Controller::registerPlugin (const std::string_view tag, const PluginDetails details) {
-	DEBUG_MSG ("Controller::%s (%s, ...)\n", __func__, tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, ...)\n", __func__, tag.data ());
 	if (this->hasPluginRegistered (tag, details.id)) {
 		if (!Controller::isNewerVersion (this->getPluginDetails (tag, details.id), details)) return;
 		this->removePlugin (tag, details.id);
@@ -141,7 +140,7 @@ void Controller::registerPlugin (const std::string_view tag, const PluginDetails
 	this->registerPluginToManager (tag_str, details);
 }
 void Controller::removePlugin (const std::string_view tag, const std::string& id) {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), id.c_str ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), id);
 	if (this->_destructor_called) return;
 
 	for (auto it = this->_registered_plugins.cbegin (); it != this->_registered_plugins.cend (); ++it) {
@@ -157,6 +156,7 @@ void Controller::removePlugin (const std::string_view tag, const std::string& id
 }
 
 bool Controller::isNewerVersion (const PluginDetails& old_plugin, const PluginDetails& new_plugin) {
+	DEBUG_MSG ("Controller::{:s} (.., ...)\n", __func__);
 	if (old_plugin.major_version < new_plugin.major_version) return true;
 	else if (old_plugin.major_version == new_plugin.major_version &&
 		old_plugin.minor_version < new_plugin.minor_version) return true;
@@ -164,7 +164,7 @@ bool Controller::isNewerVersion (const PluginDetails& old_plugin, const PluginDe
 }
 
 const std::vector<std::string> Controller::getTags (const bool& registered) const noexcept {
-	DEBUG_MSG ("Controller::%s (%d)\n", __func__, registered);
+	DEBUG_MSG ("Controller::{:s} ({:d})\n", __func__, registered);
 
 	std::vector<std::string> ret;
 	const auto contains = [] (const std::vector<std::string>& vector, const std::string& string) -> bool {
@@ -186,7 +186,7 @@ const std::vector<std::string> Controller::getTags (const bool& registered) cons
 }
 
 const std::vector<std::string> Controller::getIds (const std::string_view tag) const noexcept {
-	DEBUG_MSG ("Controller::%s (%s)\n", __func__, tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s})\n", __func__, tag.data ());
 	std::vector<std::string> ret;
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0)
@@ -196,12 +196,12 @@ const std::vector<std::string> Controller::getIds (const std::string_view tag) c
 }
 
 bool Controller::hasManager (const std::string_view tag) const noexcept {
-	DEBUG_MSG ("Controller::%s (%s)\n", __func__, tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s})\n", __func__, tag.data ());
 	return this->_managers.contains (std::string (tag));
 }
 
 const std::string_view Controller::getName (const std::string_view tag, const std::string_view id) const {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0)
 			return std::string_view (entry.second.name);
@@ -210,7 +210,7 @@ const std::string_view Controller::getName (const std::string_view tag, const st
 }
 
 const std::string_view Controller::getAuthor (const std::string_view tag, const std::string_view id) const {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0)
 			return std::string_view (entry.second.author);
@@ -219,7 +219,7 @@ const std::string_view Controller::getAuthor (const std::string_view tag, const 
 }
 
 const std::string_view Controller::getLicense (const std::string_view tag, const std::string_view id) const {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0)
 			return std::string_view (entry.second.license);
@@ -228,7 +228,7 @@ const std::string_view Controller::getLicense (const std::string_view tag, const
 }
 
 uint8_t Controller::getMajorVersion (const std::string_view tag, const std::string_view id) const {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0)
 			return entry.second.major_version;
@@ -237,7 +237,7 @@ uint8_t Controller::getMajorVersion (const std::string_view tag, const std::stri
 }
 
 uint8_t Controller::getMinorVersion (const std::string_view tag, const std::string_view id) const {
-	DEBUG_MSG ("Controller::%s (%s, %s)\n", __func__, tag.data (), tag.data ());
+	DEBUG_MSG ("Controller::{:s} ({:s}, {:s})\n", __func__, tag.data (), tag.data ());
 	for (const auto& entry : this->_registered_plugins) {
 		if (tag.compare (entry.first) == 0 && id.compare (entry.second.id) == 0)
 			return entry.second.minor_version;
